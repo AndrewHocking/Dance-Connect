c2d887d5beadb3cdbba5422d6cbb9219490a4252
Andrew Hocking <github@hocking.tech>
2024-03-15 14:16:51 -0400

    Minor visual cleanup

--- website/__init__.py
+++ website/__init__.py
@@ -34,7 +34,7 @@ def json_response(status_code: int, message: str, data: Any = None):
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs' #TODO: Change this
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
