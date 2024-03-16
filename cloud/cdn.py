454fada2b770e31331853e5f6786d69704d30ca2
ryan-pelchat <gitryanpelchat@gmail.com>
2024-03-16 14:04:11 -0400

    added more methods
    empty temp folder
    download original picture

--- cloud/cdn.py
+++ cloud/cdn.py
@@ -1,6 +1,9 @@
 # Path: cloud/cdn.py
 import requests
 import json
+import uuid
+import os
+import shutil
 
 
 class CDN:
@@ -108,7 +111,7 @@ class CDN:
         response = requests.delete(upload_url, headers=header)
         return response.json()
 
-    def get_link(self, image_id: str) -> dict:
+    def get_image_details(self, image_id: str) -> dict:
         """
         Gets a link to a file from cloudflare's image CDN.
 
@@ -148,10 +151,67 @@ class CDN:
         response = requests.get(upload_url, headers=header)
         return response.json()
 
+    def download_original(self, image_id: str) -> str:
+        """
+        Downloads the original file from cloudflare's image CDN and saves it within a temporary folder.
+        Please delete file in temporary folder after use!!! (CDN.empty_temp_folder())
+
+        Args:
+            image_id (str): The ID of the image to download.
+
+        Returns:
+            str: The path to the downloaded file.
+        """
+        # uuid is used to generate a unique file name
+        fileName = (
+            str(uuid.uuid4()) + self.get_image_details(image_id)["result"]["filename"]
+        )
+
+        path = "./cloud/temp/{}".format(fileName)
+
+        upload_url = "https://api.cloudflare.com/client/v4/accounts/{account_id}/images/v1/{image_id}/blob".format(
+            account_id=self._account_hash, image_id=image_id
+        )
+        header = {
+            "Authorization": "Bearer {apiToken}".format(apiToken=self._key),
+        }
+        blob = requests.get(upload_url, headers=header)
+        print(type(blob.content))
+
+        with open(path, "wb") as bn_fl:
+            bn_fl.write(blob.content)
+            bn_fl.close()
+
+        return path
+
+    def empty_temp_folder(self):
+        """
+        Empties the temporary folder used to store downloaded files.
+
+        Returns:
+            None
+        """
+        folder = "./cloud/temp/"
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(
+                        file_path
+                    )  # This will remove a folder and all its contents
+            except Exception as e:
+                print("Failed to delete %s. Reason: %s" % (file_path, e))
+
 
 def main():
+    """
+    The main function used for manually testing the CDN class.
+    """
     cdn = CDN()
-    cdn.upload()
+    # cdn.empty_temp_folder()
+    # print(cdn.download_original("57ca5a8c-f909-429d-d4cb-4b00b75f6d00"))
 
 
 if __name__ == "__main__":
