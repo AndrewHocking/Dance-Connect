0fb761e094af1dd225fdeafe9801cc69b3edd2f3
ryan-pelchat <gitryanpelchat@gmail.com>
2024-04-25 23:45:09 -0400

    switched to production

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ def urlencode_filter(s):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8123, debug=True)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8123)
+    # app.run(host="0.0.0.0", port=8123, debug=True)
+    # app.run(debug=True)
     app.jinja_env.filters['quote_plus'] = lambda x: quote_plus(
         str(x)) if x else ''
